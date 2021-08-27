fd4c6f305c129b3f605829fa51df0c59ee05aa34
Techstellar <olasoji@olasojiopeyemi.com>
2021-08-27 08:33:29 -0400

    changing in a branch

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 def main(msg):
-// praise be GOd
+// praise be God
     print(msg)
-
+def secondary():
+    print('New function')
 main("Hello People, are you ready?"
      "Blessed are you O LORD, for you have made all things good")
