25cd309f9c4dd1f289f925443b4333978f8d4f56
Olasoji <65076620+Sojstyles@users.noreply.github.com>
2021-08-27 09:08:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,7 @@ def main(msg):
     print(msg)
 def secondary():
     print('New function')
+    def secondary():
+    print('Second function')
 main("Hello People, are you ready?"
      "Blessed are you O LORD, for you have made all things good")
