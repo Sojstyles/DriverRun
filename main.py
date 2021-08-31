b3c214f56b624b10d8fe23c4de2fe13ae1d05e41
Techstellar <olasoji@olasojiopeyemi.com>
2021-08-30 22:45:39 -0400

    changing main.py for a sec

--- main.py
+++ main.py
@@ -8,4 +8,6 @@ def secondary():
 def third():
     print('Second function, this is just to check for further analysis')
 main("Hello People, are you ready?"
-     "Blessed are you O LORD, for you have made all things good")
+     "Blessed are you O LORD, for you have made all things good
+                        ***                         ")
+                        ***
